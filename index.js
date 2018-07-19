f1e30d52a1832c74713890c1fec6209d27999af9
Eliana K <ekirshe@gmail.com>
2018-07-19 21:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  return {delete newRecipes.prop;};
+  return delete newRecipes.prop;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
