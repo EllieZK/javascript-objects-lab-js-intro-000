0685263c1f20420b18b4e0501fe38fd954e8e969
Eliana K <ekirshe@gmail.com>
2018-07-19 21:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  return delete recipes.prop;
+  delete recipes[prop]; return recipes;};
 }
+
