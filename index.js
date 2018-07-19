043665a67b345cf48ea734ae16a9b2212d1ad5f9
Eliana K <ekirshe@gmail.com>
2018-07-19 20:38:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var recipes = {prop1: 1};
+var recipes = {prop: 1};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object.assign ({}, recipes, {['prop 2']:2} );
+  return object.assign ({}, recipes, {[prop2]: 2} );
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
@@ -13,5 +13,5 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  return delete recipes.prop1
+  return delete recipes.prop
 }
